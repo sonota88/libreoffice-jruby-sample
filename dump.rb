e630534f538394c3d1d43aecf945dfdecfc95e50
sonota88 <yosiot8753@gmail.com>
2021-04-29 06:17:38 +0900

    formula の先頭のシングルクォートを unescape (dump.rb)

--- dump.rb
+++ dump.rb
@@ -4,6 +4,15 @@ require "json"
 
 require_relative "libo_calc"
 
+def unescape_formula(formula)
+  if formula.start_with?("'")
+    # https://wiki.documentfoundation.org/EscapingCharacters#Entering_a_single_quotation_mark_as_the_first_character_of_a_formula
+    formula[1..-1]
+  else
+    formula
+  end
+end
+
 def main(file, sheet_name)
   Calc.open(file) do |doc|
     sheet = doc.get_sheet_by_name(sheet_name)
@@ -11,7 +20,8 @@ def main(file, sheet_name)
     rows =
       (0..(sheet.used_row_index_max)).map do |ri|
         (0..(sheet.used_column_index_max)).map do |ci|
-          sheet.get(ci, ri)
+          str = sheet.get(ci, ri)
+          unescape_formula(str)
         end
       end
 
